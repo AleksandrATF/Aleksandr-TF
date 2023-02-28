a4f682034742cf88d5acf4a08b06f3382d0c7cfe
АлександрАТФ <trofimovaf@vk.com>
2023-02-28 20:46:09 +0300

    промежуточная аттестация

--- js/server.js
+++ js/server.js
@@ -6,6 +6,13 @@ const port = "8000";
 
 //http://localhost:8000
 
-function (req, res) {
+const requestListener = function (req, res) {
     res.writeHead(200)
+    res.end ("Hello, server working well!");
+
+}
+
+const server = http.createServer(requestListener);
+server.listen(port, host () => {
+    console.log('Сервер запущен на http://${host}:');
 }
